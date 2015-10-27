112d2f7c0643fa7d7d1c72003be32daa2ffad6f8
urosjarc <urosjarc@users.noreply.github.com>
2015-10-27 13:08:35 +0100

    Adding grunt build, clean and unittests

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,30 +1,58 @@
 // Generated on 2015-10-23 using generator-angular-fullstack 2.1.1
 'use strict';
-
 module.exports = function (grunt) {
 
-  // Time how long tasks take. Can help when optimizing build times
-  require('time-grunt')(grunt);
+    require("load-grunt-tasks")(grunt);
 
-  // Add the grunt-mocha-test tasks.
-  grunt.loadNpmTasks('grunt-mocha-test');
+    // Time how long tasks take. Can help when optimizing build times
+    require('time-grunt')(grunt);
 
-  grunt.initConfig({
-    // Configure a mochaTest task
-    mochaTest: {
-      test: {
-        options: {
-          reporter: 'spec'
+    // Add the grunt-mocha-test tasks.
+    grunt.loadNpmTasks('grunt-mocha-test');
+
+    grunt.initConfig({
+        // Configure a mochaTest task
+        mochaTest: {
+            test: {
+                options: {
+                    reporter: 'spec'
+                },
+                src: ['test/**/*.js']
+            }
         },
-        src: ['test/**/*.js']
-      }
-    }
-  });
 
-  grunt.registerTask('test', function (target) {
-    return grunt.task.run([
-      'mochaTest'
-    ]);
-  });
+        clean: {
+            lib: 'lib/*'
+        },
 
+        babel: {
+            options: {
+                sourceMap: true,
+                experimental: true,
+                modules: "amd"        //This is the line to be added.
+            },
+            dist: {
+                files: [{
+                    "expand": true,
+                    "cwd": "src/",
+                    "src": ["**/*.js"],
+                    "dest": "lib/",
+                    "ext": ".js"
+                }]
+            }
+        }
+    });
+
+    grunt.registerTask('test', function () {
+        return grunt.task.run([
+            'mochaTest'
+        ]);
+    });
+
+    grunt.registerTask("build", function () {
+        return grunt.task.run([
+            'clean:lib',
+            'babel:dist'
+        ]);
+    });
 };
