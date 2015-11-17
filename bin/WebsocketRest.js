9beda6fc22f2f06465563478b64d696ac7aee279
urosjarc <urosjarc@users.noreply.github.com>
2015-11-17 11:21:04 +0100

    Removing consoling

--- bin/WebsocketRest.js
+++ bin/WebsocketRest.js
@@ -66,8 +66,6 @@ class WebsocketRest {
             socket.on('message', function (msg) {
                 var req = JSON.parse(msg || "{}");
 
-				console.error(msg);
-
 				//check req
                 var reqKeys = ['module', 'method'];
                 var keyError = [];
