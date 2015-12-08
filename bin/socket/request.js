ae6facc4417b47a250699a81397ff755f267246f
urosjarc <urosjarc@users.noreply.github.com>
2015-12-08 14:47:07 +0100

    Fast update

--- bin/socket/request.js
+++ bin/socket/request.js
@@ -1,7 +1,25 @@
 
 module.exports = function (socket, apiVersion, logger) {
 
-	//Create simmilar lib with collaboration with socket
-	//socket.test = new Test(req,res,logger);
+	socket.validationError = function (error) {
+		let json = {
+			apiVersion: apiVersion,
+			method: this.REST.method,
+			module: this.REST.module,
+			code: 400,
+			error: error
+		};
+		logger.debug('websocket-rest (socket.validationError)', json);
+		try {
+			this.send(JSON.stringify(json));
+		} catch (err) {
+			logger.debug('websocket-rest (socket.send)', {
+				message: 'Could not send to client',
+				json: json,
+				method: 'data',
+				stack: err
+			})
+		}
+	};
 
 };
